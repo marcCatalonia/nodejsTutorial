61dd0bfeec7433bef1b8256af104d157fdeaf040
Marc <marc.purgimon@gmail.com>
2020-04-11 19:05:00 +0200

    Blocking and non-Blocking: Asyncronous

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 //url of the module doncs : https://nodejs.org/docs/latest-v12.x/api/fs.html
 const fs = require('fs');
 
+
+//Blocking, syncronous way
 //Call to the function to read the file. We pass the path and the encoding option as a string
 const textIn = fs.readFileSync('./final/txt/input.txt', 'utf-8');
 console.log(textIn);
@@ -12,3 +14,34 @@ const textOut = `This is what weknow about avocado:  ${textIn}.\nCreated on ${Da
 //We use the function to write at the file called output
 fs.writeFileSync('./final/txt/output.txt', textOut);
 console.log('File written!');
+
+//Non-Blocking, asyncronous way
+fs.readFile('./final/txt/start.txt', 'utf-8', (err, data) =>{
+    console.log(data);
+});
+console.log('Will read file');
+
+//Callback Hell
+fs.readFile('./final/txt/start.txt', 'utf-8', (err, data) =>{
+    fs.readFile(`./final/txt/${data}.txt`, 'utf-8', (err, data2) =>{
+        console.log(data2);
+    });
+});
+
+console.log('Callback hell 1');
+
+fs.readFile('./final/txt/start.txt', 'utf-8', (err, data) =>{
+    fs.readFile(`./final/txt/${data}.txt`, 'utf-8', (err, data2) =>{
+        console.log(data2);
+        fs.readFile(`./final/txt/append.txt`, 'utf-8', (err, data3) =>{
+            console.log(data3);
+
+            //finally we will write asyncronously to a file
+            fs.writeFile('./final/txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+                console.log('Your file has been written ');
+            });
+        });
+    });
+});
+
+console.log('Callback hell 2');
