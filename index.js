d93996a58e32e63c1c62e7ba66900997307f4797
Marc <marc.purgimon@gmail.com>
2020-04-11 18:21:09 +0200

    Reading and Writing fils

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 //Require fs (file system) module saved on fs variable
 //url of the module doncs : https://nodejs.org/docs/latest-v12.x/api/fs.html
 const fs = require('fs');
-const hello = 'Hello world';
-console.log(hello);
+
+//Call to the function to read the file. We pass the path and the encoding option as a string
+const textIn = fs.readFileSync('./final/txt/input.txt', 'utf-8');
+console.log(textIn);
+
+//String saved in a const
+const textOut = `This is what weknow about avocado:  ${textIn}.\nCreated on ${Date.now()}`;
+
+//We use the function to write at the file called output
+fs.writeFileSync('./final/txt/output.txt', textOut);
+console.log('File written!');
