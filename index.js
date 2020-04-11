55d154f88ad84100973c1ab70dbf0721ba7b9185
Marc <marc.purgimon@gmail.com>
2020-04-11 17:37:27 +0200

    Using Modules 1: Core Modules

---
+++ index.js
@@ -0,0 +1,5 @@
+//Require fs (file system) module saved on fs variable
+//url of the module doncs : https://nodejs.org/docs/latest-v12.x/api/fs.html
+const fs = require('fs');
+const hello = 'Hello world';
+console.log(hello);
