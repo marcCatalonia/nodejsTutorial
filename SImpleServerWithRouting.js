fb8f10a60ee44f4020da5933a442d9c97d50057c
Marc <marc.purgimon@gmail.com>
2020-04-13 18:26:19 +0200

    Building a Very Simple API

--- SImpleServerWithRouting.js
+++ SImpleServerWithRouting.js
@@ -2,6 +2,11 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+
+//We read at the begining the data.json syncronously and then we won't have to read it againn for each time someone needs it
+const data = fs.readFileSync(`${__dirname}/final/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
     console.log(req.url);
 
@@ -11,6 +16,13 @@ const server = http.createServer((req, res) => {
 
     else if (pathName === '/product')
     res.end('This is the product');
+
+    else if(pathName === '/api'){
+        //Read the JSON file
+            res.writeHead(200, {'Content-type': 'application/json'});
+            res.end(data);
+    }
+        
     
     else{
             res.writeHead(404, {
